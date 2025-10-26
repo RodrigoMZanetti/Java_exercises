569f31ad7d989773865b8735eb57776ee0ee5b44
Rodrigo Maturano Zanetti <rodrigogitzanetti@outlook.com>
2025-10-26 11:09:12 -0300

    First exercise

---
+++ HoraDoSistema.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class HoraDoSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Date data = new Date();
+        System.out.println("A hora do sistema é");
+        System.out.println(data.toString());
+    }
+    
+}
